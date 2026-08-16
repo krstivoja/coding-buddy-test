0238471ed848cbf5e6e01534e95f0bba97b9fd1f
Marko Krstic <mailme@markokrstic.com>
2026-08-16 18:40:01 +0200

    Create coding-buddy-test.php

---
+++ coding-buddy-test.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:       Coding Buddy Test
+ * Plugin URI:        https://github.com/krstivoja/coding-buddy-test
+ * Description:       Sandbox plugin for exercising coding-buddy. Nothing ships from here.
+ * Version:           0.1.0
+ * Requires at least: 6.4
+ * Requires PHP:      8.1
+ * Author:            DPlugins
+ * License:           GPL-2.0-or-later
+ * Text Domain:       coding-buddy-test
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
